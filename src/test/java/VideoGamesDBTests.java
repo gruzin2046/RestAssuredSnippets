09c847e387fe49e18cfc847cf00e73d229a2de42
aluniewi <albert.luniewicz@capgemini.com>
2022-11-10 13:40:56 +0100

    serialization

--- src/test/java/VideoGamesDBTests.java
+++ src/test/java/VideoGamesDBTests.java
@@ -8,14 +8,30 @@ import static io.restassured.RestAssured.given;
 
 public class VideoGamesDBTests extends VideoGameConfig {
 
+    // 1.a. GET method (all collection)
+    // Request URI: http://localhost:8080/app/videogames
     @Test
     public void getAllGames() {
         get(ALL_VIDEO_GAMES.getEndpoint());
     }
 
+    // 1.b. GET method (single item) using pathParam(String paramName,Object paramValue)
+    // Request URI: http://localhost:8080/app/videogames/{videoGameId} ->
+    //              http://localhost:8080/app/videogames/1
+
+    @Test
+    public void getSingleGame() {
+        given()
+                .pathParam("videoGameId", 1).
+        when()
+                .get(SINGLE_VIDEO_GAME.getEndpoint()).
+        then();
+    }
+
+    // 2.a. POST method - JSON format
     @Test
     public void addNewGameByJSON() {
-        String requestBodyJSON = "{\"id\": 100,\n" +
+        String requestBodyJSON = "{\"id\": 99,\n" +
                 "            \"name\": \"Lord of the rings\",\n" +
                 "            \"releaseDate\": \"2022-11-04T15:43:34.800Z\",\n" +
                 "            \"reviewScore\": 10,\n" +
@@ -23,22 +39,25 @@ public class VideoGamesDBTests extends VideoGameConfig {
                 "            \"rating\": \"Universal\"}";
 
         given()
-                .body(requestBodyJSON)
-                .when()
-                .post(ALL_VIDEO_GAMES.getEndpoint())
-                .then();
+                .body(requestBodyJSON).
+        when()
+                .post(ALL_VIDEO_GAMES.getEndpoint()).
+        then();
 
 
-        /*{
+        /*
+            {
             "id": 99,
             "name": "Lord of the rings",
             "releaseDate": "2022-11-04T15:43:34.800Z",
             "reviewScore": 10,
             "category": "fantasy",
             "rating": "Universal"
-            }*/
+            }
+        */
     }
 
+    // 2.b. POST method - XML format
     @Test
     public void addNewGameByXML() {
         String requestBody = "  <videoGame category=\"Shooter\" rating=\"Universal\">\n" +
@@ -50,15 +69,16 @@ public class VideoGamesDBTests extends VideoGameConfig {
         given()
                 .body(requestBody)
                 .contentType("application/xml")
-                .accept("application/xml")
-                .when()
-                .post(ALL_VIDEO_GAMES.getEndpoint())
-                .then();
+                .accept("application/xml").
+        when()
+                .post(ALL_VIDEO_GAMES.getEndpoint()).
+        then();
     }
 
+    // 3. PUT method
     @Test
     public void updateGame() {
-        String id = "2";
+        int id = 3;
         String requestBody = "{\n" +
                 "  \"id\": " + id + ",\n" +
                 "  \"name\": \"asdasdasd\",\n" +
@@ -68,32 +88,46 @@ public class VideoGamesDBTests extends VideoGameConfig {
                 "  \"rating\": \"string\"\n" +
                 "}";
 
-        String endpoint = SINGLE_VIDEO_GAME
-                .getEndpoint()
-                .replace("{videoGameId}", id);
-
         given()
-                .body(requestBody)
-                .when()
-                .put(endpoint)
-                .then();
+                .pathParam("videoGameId", 3)
+                .body(requestBody).
+        when()
+                .put(SINGLE_VIDEO_GAME.getEndpoint()).
+        then();
     }
 
+    // 4. DELETE method
     @Test
     public void deleteGame() {
-        given()
-                .when()
-                .delete("videogames/6")
-                .then();
+        given().
+        when()
+                .delete("videogames/6").
+        then();
 
     }
 
+    // serialization steps
+    // How to send POJO (plain old java object)
+    // 1. get JSON response body from get single game request call
+    // 2. generate POJO using http://pojo.sodhanalibrary.com/ online tool
+    // 3. prepare VideoGame java class for single game representation
+    // 4. create new VideoGame instance
+    // 5. send this instance as request body
+
     @Test
-    public void getSingleGame() {
+    public void testSerializationByJSON() {
+        VideoGame videoGame = new VideoGame("99", "2010", "awesome game", "any", "44", "shooter");
+
         given()
-                .pathParam("videoGameId", 1).
+                .body(videoGame).
         when()
-                .get(SINGLE_VIDEO_GAME.getEndpoint()).
+                .post(ALL_VIDEO_GAMES.getEndpoint()).
         then();
+
+        // response:
+
+        // {
+        //    "status": "Record Added Successfully"
+        // }
     }
 }
